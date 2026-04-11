7344ec74c5aaa26949a44f810b245ca78ed033e9
Jesse Mattila <e2402265@edu.vamk.fi>
2026-04-11 11:34:07 +0300

    kirjotin App.tsx tiedostoon tekstiä

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
       <h1 className="text-center bg-red-400 text-4xl py-4">Template for the TechBurger project</h1>
+      <div className="bg-blue-500 text-white p-4">Jesse tässä mooooi</div>
     </>
   )
 }
