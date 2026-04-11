02ee42d09c936894f1e6bf3768f98cff7adafb2c
MPerttu <mika.perttu@gmail.com>
2026-04-11 11:49:33 +0300

    Lisätty nimitagi etusivulle (Mika Perttu)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     <>
       <h1 className="text-center bg-red-400 text-4xl py-4">Template for the TechBurger project</h1>
       <div className="bg-blue-500 text-white p-4">Jesse tässä mooooi</div>
+      <div className="bg-yellow-500 text-white p-4">Mika Perttu</div>
     </>
   )
 }
