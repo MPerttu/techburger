ff52724b9ab009d79b11a900ef2a3d13c95cd328
MPerttu <mika.perttu@gmail.com>
2026-04-18 10:56:27 +0300

    Header-komponentti luotu oranssilla taustalla

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { Header } from './components/Header';
 
 function App() {
-
   return (
-    <>
-      <h1 className="text-center bg-red-400 text-4xl py-4">Template for the TechBurger project</h1>
-      <div className="bg-blue-500 text-white p-4">Jesse tässä mooooi</div>
-      <div className="bg-yellow-500 text-white p-4">Mika Perttu</div>
-    </>
-  )
+    <div className="min-h-screen bg-slate-50">
+      <Header />
+      <main className="p-8">
+        {/* Tänne tulee myöhemmin tuotelistaus */}
+        <h2 className="text-xl text-slate-700">Welcome to TechBurger!</h2>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
