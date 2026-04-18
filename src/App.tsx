3be1e1e27e2b379ba089b7b436eaf4b53286c6c3
MPerttu <mika.perttu@gmail.com>
2026-04-18 18:54:36 +0300

    Toteutettu ProductList-komponentti ja lisätty testidata

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { ProductList } from "./components/ProductList";
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-50">
       <Header />
+    
 
       <main className="p-8">
-        {/* Tänne tulee myöhemmin tuotelistaus */}
         <h2 className="text-xl text-slate-700">Welcome to TechBurger!</h2>
+        <ProductList />
       </main>
       <Footer />
     </div>
