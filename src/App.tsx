73e402424dea57a713960deaab78abce997557e6
vadim-horisontti <va.kliuchevskii@gmail.com>
2026-04-18 11:15:04 +0300

    Add Footer Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { Header } from './components/Header';
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-50">
       <Header />
+
       <main className="p-8">
         {/* Tänne tulee myöhemmin tuotelistaus */}
         <h2 className="text-xl text-slate-700">Welcome to TechBurger!</h2>
       </main>
+      <Footer />
     </div>
   );
 }
